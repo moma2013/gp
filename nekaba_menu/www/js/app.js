10f4481adb9703e0c195d2d99b5b0b9ef539029e
ManarAhmed <eng.mema2014@yahoo.com.au>
2016-05-22 16:47:32 +0200

    adjust websql database

--- nekaba_menu/www/js/app.js
+++ nekaba_menu/www/js/app.js
@@ -4,9 +4,10 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+var db = null;
+angular.module('starter', ['ionic','ngCordova', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaSQLite) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -19,6 +20,13 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+//    db = $cordovaSQLite.openDB({ name : "my.db" ,loction: "default" });
+//    $cordovaSQLite.excute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)" );
+ db =openDatabase('mydb','1.0','Test DB',2*1024*1024);
+ db.transaction(function(tx){
+      tx.executeSql("CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
+    });
+   
   });
 })
 
@@ -98,11 +106,51 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       } 
     }
     })
+    .state('app.sqlite', {
+    url: '/sqlite',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/sqlite.html',
+        controller: 'ExampleController'
+    }}
+    })
     .state('complaint', {
     url: '/complaint/:complaintId',
         templateUrl: 'templates/complaint.html',
         controller: 'ComplaintsCtrl'
-    });
+    })
+   ;
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/profile');
-});
+})
+.controller("ExampleController", function($scope, $cordovaSQLite) {
+ 
+    $scope.insert = function(firstname, lastname) {
+        
+//        var query = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
+//        $cordovaSQLite.execute(db, query, [firstname, lastname]).then(function(res) {
+//            alert("INSERT ID -> " + res.insertId);
+//            console.log("INSERT ID -> " + res.insertId);
+//        }, function (err) {
+//            console.error(err);
+//        });
+ db.transaction(function(tx){
+      tx.executeSql("'INSERT INTO LOGS (id,log) VALUES (?, ?)",[firstname,lastname]);
+       alert(firstname+":"+firstname);
+    });
+    }
+ 
+//    $scope.select = function(lastname) {
+//        var query = "SELECT firstname, lastname FROM people WHERE lastname = ?";
+//        $cordovaSQLite.execute(db, query, [lastname]).then(function(res) {
+//            if(res.rows.length > 0) {
+//                console.log("SELECTED -> " + res.rows.item(0).firstname + " " + res.rows.item(0).lastname);
+//            } else {
+//                console.log("No results found");
+//            }
+//        }, function (err) {
+//            console.error(err);
+//        });
+//    }
+ 
+});;
